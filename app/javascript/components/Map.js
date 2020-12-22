b1f48ed3ea64775f35d764bbd7859572c7c034b1
Grant Holley <gvholley@gmail.com>
2020-12-22 22:32:14 +0900

    Map display

--- app/javascript/components/Map.js
+++ app/javascript/components/Map.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-export default class Map extends React.Component {
+class Map extends React.Component {
  render() {
    return (
      <h1>Hello From Map Component</h1>
    )
  }
 }
+
+export default Map;
